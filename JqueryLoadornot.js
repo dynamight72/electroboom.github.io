ae836439b3f6c437f9f073cad7500247a9cc6354
ysiddhanth <turboster.flash@gmail.com>
2021-05-16 15:58:31 +0530

    master commit

---
+++ JqueryLoadornot.js
@@ -0,0 +1,10 @@
+if(typeof jQuery == "undefined") {
+
+    alert("jQuery is not loaded in this page");
+
+}
+else(typeof jQuery == "defined"); {
+    
+    alert("jQuery is loaded successfully");
+
+}
